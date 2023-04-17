ecbb1577fcbb3deeeba71799af7ef317458abbc0
joynalabedin05 <joynal05101993@gmail.com>
2023-04-17 21:27:01 +0600

    implemented login and register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,8 +4,8 @@ import { AuthContext } from '../providers/AuthProviders';
 
 const Register = () => {
 
-    const {user} = useContext(AuthContext);
-    console.log(user);
+    const {user, createUser} = useContext(AuthContext);
+    // console.log(user, createUser);
 
     const handleRegister = event =>{
         event.preventDefault();
@@ -14,6 +14,15 @@ const Register = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(name, email, password);
+        createUser(email, password)
+        .then(result=>{
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            form.reset();
+        })
+        .catch(error =>{
+            console.log(error);
+        })
     }
 
     return (
@@ -46,7 +55,7 @@ const Register = () => {
                     </label>
                 </div>
                 <div className="form-control mt-6">
-                    <button className="btn btn-primary">Login</button>
+                    <button className="btn btn-primary">Register</button>
                 </div>
             </form>
         </div>
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState } from 'react';
 export const AuthContext = createContext(null);
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 
@@ -9,9 +9,18 @@ const auth = getAuth(app);
 const AuthProviders = ({children}) => { 
 
     const [user, setUser] = useState(null);
+    const createUser = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) =>{
+       return signInWithEmailAndPassword(auth, email, password);
+    }
     
     const authInfo = {
         user,
+        createUser,
+        signIn,
     }
     return (
         <AuthContext.Provider value={authInfo}>
